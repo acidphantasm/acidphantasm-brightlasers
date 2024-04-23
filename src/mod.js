2bc9ed85df83dc72d324e862050837c0e47d97a4
acidphantasm <127812106+acidphantasm@users.noreply.github.com>
2024-04-23 18:13:15 -0500

    adjust logging

--- src/mod.js
+++ src/mod.js
@@ -1,10 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Mod {
+
+    constructor() 
+    {
+        this.mod = "acidphantasm-brightlasers"; // Set name of mod so we can log it to console later
+    }
+
     postAkiLoad(container) {
         // get the logger from the server container
         const logger = container.resolve("WinstonLogger");
-        logger.info("Loading: Bundle Loading");
+        logger.info(`[${this.mod}] Loading: Laser Bundle`);
     }
 }
 module.exports = { mod: new Mod() };
